28d791749ace341a9c27812308e9a921df9881b9
Emmanuel Narcisse <epokweb@hotmail.fr>
2021-09-20 09:23:27 +0400

    Style(mobile-first) & init MVC pattern

---
+++ model/photographerModel.js
@@ -0,0 +1,16 @@
+
+
+export default class photographers {
+    constructor(name, id, city, country, tags, tagline, price, portrait) {
+        this._name = name;
+        this._id = id;
+        this._city = city;
+        this._country = country;
+        this._tags = tags;
+        this._tagline = tagline;
+        this._price = price;
+        this._portrait = portrait;
+    }
+
+
+}
